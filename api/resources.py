bce2b24bab18f039af3c7734fca4122aef1ddd3f
badreddinesouhar <badrsouhar@gmail.com>
2024-01-05 11:54:11 +0100

    django api

---
+++ api/resources.py
@@ -0,0 +1,19 @@
+#api/resources.py
+
+from tastypie.resources import ModelResource
+from api.models import Note
+
+class NoteResource(ModelResource):
+    class Meta:
+        queryset = Note.objects.all()
+        resource_name = 'note'
+
+from tastypie.resources import ModelResource
+from api.models import Note
+from tastypie.authorization import Authorization
+
+class NoteResource(ModelResource):
+    class Meta:
+        queryset = Note.objects.all()
+        resource_name = 'note'
+        authorization = Authorization()
